909260574ff2788e01074059a8ff90794192708a
SalahLaouini <linaopticlunettes@gmail.com>
2025-07-29 14:31:10 +0100

    Update pages

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,15 +2,46 @@ import { Routes, Route } from "react-router-dom";
 import AuthLayout from "./components/auth/Layout.jsx";
 import AuthLogin from "./pages/auth/login.jsx";
 import AuthRegister from "./pages/auth/register.jsx";
+import AdminLayout from "./components/admin-view/Layout.jsx";
+import AdminDashboard from "./pages/admin-view/dashboard.jsx";
+import AdminProducts from "./pages/admin-view/products.jsx";
+import AdminOrders from "./pages/admin-view/orders.jsx";
+import AdminFeatures from "./pages/admin-view/features.jsx";
+import ShoppingLayout from "./components/shopping-view/Layout.jsx";
+import NotFound from "./pages/not-found/index.jsx";
+import ShoppingListing from "./pages/shopping-view/listing.jsx";
+import ShoppingCheckout from "./pages/shopping-view/checkout.jsx";
+import ShoppingAccount from "./pages/shopping-view/account.jsx";
+import ShoppingHome from "./pages/shopping-view/home.jsx";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/auth" element={<AuthLayout />}>
-        <Route path="login" element={<AuthLogin />} />
-        <Route path="register" element={<AuthRegister />} />
-      </Route>
-    </Routes>
+ return (
+    <div className="flex flex-col overflow-hidden bg-white">
+      <Routes>
+        <Route path="/auth" element={<AuthLayout />}>
+          <Route index element={<AuthLogin />} />
+          <Route path="login" element={<AuthLogin />} />
+          <Route path="register" element={<AuthRegister />} />
+        </Route>
+
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<AdminDashboard />} />
+          <Route path="dashboard" element={<AdminDashboard />} />
+          <Route path="products" element={<AdminProducts />} />
+          <Route path="orders" element={<AdminOrders />} />
+          <Route path="features" element={<AdminFeatures />} />
+        </Route>
+
+        <Route path="/shop" element={<ShoppingLayout/>} >
+        <Route path="home" element={<ShoppingHome />} />
+        <Route path="listing" element={<ShoppingListing />} />
+        <Route path="checkout" element={<ShoppingCheckout />} />
+        <Route path="account" element={<ShoppingAccount />} />
+        </Route>
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
   );
 }
 
--- Client/src/components/admin-view/Layout.jsx
+++ Client/src/components/admin-view/Layout.jsx
@@ -1,21 +1,21 @@
+import AdminSideBar from "./sidebar";
 import AdminHeader from "./header";
-import AdminSidebar from "./sidebar";
+import { Outlet } from "react-router-dom";
 
-
-function AdminLayout () {
-    return (
-        <div className="flex min-h-screen w-full">
-            
-{/* admin sidebar */}
-<AdminSidebar/>
-<div className="flex flex-1 flex-col">
-{/* admin sidebar ***/}
-<AdminHeader/>
+function AdminLayout() {
+  return (
+    <div className="flex min-h-screen w-full">
+      {/* admin sidebar */}
+      <AdminSideBar />
+      <div className="flex flex-1 flex-col">
+        {/* admin header */}
+        <AdminHeader />
         <main className="flex-1 flex bg-muted/40 p-4 md:p-6">
-
+          <Outlet/>
         </main>
-       </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 }
- export default AdminLayout;
+
+export default AdminLayout;
