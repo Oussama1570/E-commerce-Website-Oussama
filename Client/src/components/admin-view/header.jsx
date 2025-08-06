df51fe6df69bb95eb773edc319a2a1397c312094
SalahLaouini <linaopticlunettes@gmail.com>
2025-08-06 21:33:05 +0100

    Fixe Index.css for whole css

--- Client/src/components/admin-view/header.jsx
+++ Client/src/components/admin-view/header.jsx
@@ -1,39 +1,30 @@
 import { AlignJustify, LogOut } from "lucide-react";
 import { Button } from "../ui/button";
 import { useDispatch } from "react-redux";
+import { logoutUser } from "@/store/auth-slice";
 
 function AdminHeader({ setOpen }) {
   const dispatch = useDispatch();
 
-  const handleLogout = () => {
-    console.log("Logout clicked");
-    // dispatch(logoutUser());
-  };
+  function handleLogout() {
+    dispatch(logoutUser());
+  }
 
   return (
-    <header className="flex items-center justify-between px-6 py-3 border-b bg-white shadow-sm">
-      {/* Left side: Burger icon for mobile */}
-      <Button
-        onClick={() => setOpen(true)}
-        className="lg:hidden"
-        variant="ghost"
-        size="icon"
-      >
+    <header className="flex items-center justify-between px-4 py-3 bg-background border-b">
+      <Button onClick={() => setOpen(true)} className="lg:hidden sm:block">
         <AlignJustify />
         <span className="sr-only">Toggle Menu</span>
       </Button>
-
-      {/* Spacer pushes logout to the right */}
-      <div className="flex-1" />
-
-      {/* Right side: Logout button */}
-      <Button
-        onClick={handleLogout}
-        className="bg-black text-white hover:bg-gray-800 px-4 py-2 rounded-md text-sm font-medium flex items-center gap-2"
-      >
-        <LogOut size={16} />
-        Logout
-      </Button>
+      <div className="flex flex-1 justify-end">
+        <Button
+          onClick={handleLogout}
+          className="inline-flex gap-2 items-center rounded-md px-4 py-2 text-sm font-medium shadow"
+        >
+          <LogOut />
+          Logout
+        </Button>
+      </div>
     </header>
   );
 }
--- Client/src/pages/admin-view/products.jsx
+++ Client/src/pages/admin-view/products.jsx
@@ -1,6 +1,16 @@
+import { Button } from "@/components/ui/button";
+import { Fragment } from "react";
+
+
 function AdminProducts() {
     return (
-        <div>admin products</div>
+        <Fragment>
+        <div className="mb-3 w-full flex justify-end">admin products
+        <button> Add New Product</button>
+
+        </div>
+        <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-4"></div>
+        </Fragment>
     );
 }
 
